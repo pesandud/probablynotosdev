967a4dc9a9786b19f3d553e4a4c34c16358ad266
kyliex7 <216264999+kyliex7@users.noreply.github.com>
2026-04-30 19:18:26 +0530

    implemented the gdt lol

---
+++ babysteps/kernel/arch/i386/gdt.c
@@ -0,0 +1,32 @@
+#include <kernel/gdt.h>
+
+#define GDT_ENTRIES 3
+
+static gdt_entry_t gdt[GDT_ENTRIES];
+static gdt_descriptor_t gdt_desc;
+
+extern void gdt_flush(uint32_t);
+
+static void set_gdt(int i, uint32_t base, uint32_t limit, uint8_t access, uint8_t gran) {
+	gdt[i].base_low = base & 0xffff;	
+	gdt[i].base_mid = (base >> 16) & 0xff;	
+	gdt[i].base_high = (base >> 24) & 0xff;	
+	gdt[i].limit_low = limit & 0xffff;
+	gdt[i].granularity = ((limit >> 16) & 0xf) | (gran & 0xf0);
+	gdt[i].access = access;
+}
+
+void gdt_init(void) {
+	gdt_desc.limit = sizeof(gdt) - 1;
+	gdt_desc.base = (uint32_t)&gdt;
+
+	set_gdt(0, 0, 0, 0, 0);
+	// kernel code n data
+	set_gdt(1, 0, 0xffffffff, 0x9a, 0xcf);
+	set_gdt(2, 0, 0xffffffff, 0x92, 0xcf);
+	// user code n data
+	set_gdt(3, 0, 0xffffffff, 0xfa, 0xcf);
+	set_gdt(4, 0, 0xffffffff, 0xf2, 0xcf);
+
+	gdt_flush((uint32_t)&gdt_desc);
+}
--- babysteps/kernel/src/kernel/kernel.c
+++ babysteps/kernel/src/kernel/kernel.c
@@ -1,7 +1,10 @@
 #include <kernel/tty.h>
+#include <kernel/gdt.h>
 
 void kmain(void) {
+  gdt_init();
   term_init();
-	term_writestr("hello, kernel world!\n");
+	term_writestr("[+] gdt loaded successfully\n");
+	term_writestr("welcome to the pesanduOS v0.0\n");
   for (;;);
 }
